46e91bb2ccb0a72b3366837c541cee9ae7cd3618
dr96mabuza <dr96mabuza@gmail.com>
2022-05-14 08:43:08 +0200

    cardDisplay displays initial values

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
+import Display from "./components/cardDisplay";
 
 function App() {
   const [memory, setMemory] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
   return (
     <div className="App">
-      <div>{memory}</div>
+      <div>
+        <Display memory={memory} />
+      </div>
     </div>
   );
 }
--- src/components/cardDisplay.js
+++ src/components/cardDisplay.js
@@ -3,7 +3,13 @@ import React from "react";
 function Display(props) {
   const { memory } = props;
 
-  return <div>memory.map(() =>{})</div>;
+  return (
+    <div>
+      {memory.map((item) => {
+        return <div>{item}</div>;
+      })}
+    </div>
+  );
 }
 
 export default Display;
