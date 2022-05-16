ba972cb491f1f56192cdca2fa326feed6558c51d
dr96mabuza <dr96mabuza@gmail.com>
2022-05-16 11:57:19 +0200

    hard coded display and added style sheet and images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React, { useState } from "react";
 import Display from "./components/cardDisplay";
 
 function App() {
-  const [memory, setMemory] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-
   return (
     <div className="App">
       <div>
-        <Display memory={memory} />
+        <Display />
       </div>
     </div>
   );
